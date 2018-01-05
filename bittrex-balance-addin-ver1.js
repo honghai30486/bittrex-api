3c3fe9e9231d0b0cb5c92e2aeaf7e7cb0aef5f23
honghai30486 <35089192+honghai30486@users.noreply.github.com>
2018-01-06 07:35:44 +0900

    Update bittrex-balance-addin-ver1.js

--- bittrex-balance-addin-ver1.js
+++ bittrex-balance-addin-ver1.js
@@ -5,21 +5,41 @@ s.type = "text/javascript";
 s.src = "https://rawgit.com/honghai30486/bittrex-api/master/bittrex-api-ver1.js";
 $("head").append(s);
 
-$("#balanceTable tbody").find("tr").each(function(){
-	var marketName = $(this).find("a").html();
-	if (marketName === undefined){
-		marketName = $(this).find("td.text").html();
-	}
-	
-	//if (marketName == "BTC" || marketName == "USDT") return;
-	
-	$(this).append("<select id='percent_"+marketName+"' style='color: black'><option value='30'>30%</option><option value='50'>50%</option><option value='80'>80%</option><option value='100' selected>100%</option></select>");
-	var buttonCancel = document.createElement("button");
-	buttonCancel.innerHTML = "CANCEL";
-	$(this).append(buttonCancel)
+var buttonLoad = document.createElement("button");
+buttonLoad.innerHTML = "TOOL";
+$("#toolbar-balances").append(buttonCancel)
+buttonLoad.addEventListener ("click", load);
 
-	var buttonSell = document.createElement("button");
-	buttonSell.innerHTML = "SELL";
-	$(this).append(buttonSell)
-});
+function load(){
+	$("#balanceTable tbody").find("tr").each(function(){
+		var marketName = $(this).find("a").html();
+		if (marketName === undefined){
+			marketName = $(this).find("td.text").html();
+		}
 		
+		if (marketName == "USDT") return;
+		
+		$(this).append("<select id='percent_"+marketName+"' style='color: black'><option value='30'>30%</option><option value='50'>50%</option><option value='80'>80%</option><option value='100' selected>100%</option></select>");
+		var buttonCancel = document.createElement("button");
+		buttonCancel.innerHTML = "CANCEL";
+		$(this).append(buttonCancel)
+		buttonSell.addEventListener ("click", function(){cancel(marketName)});
+		
+		var buttonSell = document.createElement("button");
+		buttonSell.innerHTML = "SELL";
+		$(this).append(buttonSell)
+		buttonSell.addEventListener ("click", sell);
+	});
+}
+
+function cancel(marketName){
+	console.log("Action: CANCEL");
+	getmarketsummary("BTC-"+marketName,function(data){
+		console.log(data)
+	});
+}
+
+function sell(){
+	console.log("Action: SELL");
+	
+}
